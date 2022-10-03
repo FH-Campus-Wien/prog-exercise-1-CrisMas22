6ea27cb1abd161a87773039a2a9dec58f25ef67c
Cristina <cristina.masson.del.alamo@stud.fh-campuswien.ac.at>
2022-10-03 22:22:14 +0200

    Bis 8

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -69,10 +69,11 @@ public class App {
         int two = 0;
         one = scanner.nextInt();
         two = scanner.nextInt();
+        scanner.close();
         System.out.println("x: " +"y: "+"After Swap:");
-
         System.out.println("x: " + two);
         System.out.println("y: " + one);
+
 
 
 
@@ -81,26 +82,104 @@ public class App {
     //todo Task 6
     public void compareTwoNumbers(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int n1 = 0;
+        int n2 = 0;
+        n1 = scanner.nextInt();
+        n2 = scanner.nextInt();
+        if (n1>n2) {
+            System.out.println("n1: " + "n2:" + " n1 > n2");
+        } else if (n2>n1) {
+            System.out.println("n1: " + "n2:" + " n2 > n1");
+        } else {
+            System.out.println("n1: " + "n2:" + " n1 == n2");
+        }
+            scanner.close();
+
+
+
+
+
+
+
     }
 
     //todo Task 7
     public void ratingSalesPerson(){
         // input your solution here
+
+            Scanner scanner = new Scanner(System.in);
+            int revenue = 0;
+            revenue = scanner.nextInt();
+            if (revenue < 0 || revenue >= 100000) {
+                System.out.println("Enter annual Revenue: " + "Invalid Revenue");
+            }
+            else if (0 <= revenue && revenue < 20000) {
+                System.out.println("Enter annual Revenue: " + "Poor Sales Revenue");}
+            else if (20000 <= revenue && revenue < 50000){
+                System.out.println("Enter annual Revenue: " + "Average Sales Revenue");
+            } else if (50000 <= revenue && revenue < 80000) {
+                System.out.println("Enter annual Revenue: " + "Good Sales Revenue");
+                } else if (80000 <= revenue && revenue < 100000) {
+                System.out.println("Enter annual Revenue: " + "Excellent Sales Revenue");
+            } scanner.close();
+
+
     }
 
     //todo Task 8
     public void getCommissionRate(){
         // input your solution here
+
+        Scanner scanner = new Scanner(System.in);
+        int commission;
+        commission = scanner.nextInt();
+
+        switch (commission){
+            case 1:
+                System.out.println("Enter CommissionClass: " + "Your Commission Rate was set to 0.01");
+                break;
+            case 2:
+                System.out.println("Enter CommissionClass: " + "Your Commission Rate was set to 0.02");
+                break;
+            case 3:
+                System.out.println("Enter CommissionClass: " + "Your Commission Rate was set to 0.03");
+                break;
+            case 4:
+                System.out.println("Enter CommissionClass: " + "Your Commission Rate was set to 0.04");
+                break;
+            default:
+                System.out.println("Enter CommissionClass: " + "Your Commission Rate was set to 0.0");
+                break;
+
+                }
+        scanner.close();
+
     }
 
     //todo Task 9
     public void leapyear(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        scanner.close();
+        if (year % 4 == 0){
+            System.out.println("Year: " + "Leapyear");
+        }else if ((year % 100 == 0) ){
+            System.out.println("Year: " + "Not a Leapyear" );
+        } else if (year % 400 == 0){
+            System.out.println("Year: " + "Leapyear");
+        }
+
+
     }
 
     //todo Task 10
     public void transposedNumbers(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+
+
 
     }
 
